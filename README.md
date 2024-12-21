0a34750d2a22d2cf44c8ffbde81d4f9b5a614d04
Biswajit Basak <juelbasak@gmail.com>
2024-12-21 21:30:54 +0530

    Added comments

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 1. The user should be able to enter their First Name, Last Name and email.
 2. The user should be able to submit their details into the System.
 3. The user should be able to cancel their submission.
-4. The user should be able to view the other user's information.
+4. The user should be able to view the other user's submitted information.
 
--- static/script.js
+++ static/script.js
@@ -1,5 +1,9 @@
+// Author: Biswajit Basak
+
+// Stores the table headings
 const tableHeading = ['#', 'First Name', 'Last Name', 'Email', 'Action'];
 
+// Stores the number of users created
 let userCount = 0;
 
 const statusBarDiv = document.createElement('div');
@@ -10,11 +14,12 @@ statusBarDiv.appendChild(statusMessage);
 document.addEventListener('DOMContentLoaded', loadMainPage);
 
 
-// Helper Methods
+// This function will replace space with hyphen and convert the string to lower case
 function replaceSpaceWithhyphenAndConvertToLower(value) {
     return value.toLowerCase().replace(' ', '-');
 }
 
+// This function takes in a list of button values and creates a button element for each value
 function buttonDivFunction(listOfButtonValues) {
     let buttonDiv = document.createElement('td');
     listOfButtonValues.forEach(value => {
@@ -26,18 +31,21 @@ function buttonDivFunction(listOfButtonValues) {
     return buttonDiv;
 }
 
+// This function will create the submit and cancel button
 function addSubmitAndCancelButtonFunction() {
     const listOfButtonValues = ['Submit', 'Cancel'];
     const actionRow = buttonDivFunction(listOfButtonValues);
     return actionRow;
 }
 
+// This function will create the update and delete button
 function addUpdateAndDeleteButtonFunction() {
     const listOfButtonValues = ['Update', 'Delete'];
     const actionRow = buttonDivFunction(listOfButtonValues);
     return actionRow;
 }
 
+// This function will create the action row consisting of different buttons
 function createActionRow(addSubmitAndCancelButton = false, addUpdateAndDeleteButton = false) {
     let actionRow;
     if (addSubmitAndCancelButton) {
@@ -49,10 +57,12 @@ function createActionRow(addSubmitAndCancelButton = false, addUpdateAndDeleteBut
     return actionRow
 }
 
+// This function will set the status message
 function setStatusMessage(message) {
     statusMessage.innerText = 'Status: ' + message;
 }
 
+// This function will create the heading of the table
 function createUserInputHeadingFunction() {
     let tableHeadingDiv = document.createElement('tr');
 
@@ -64,6 +74,7 @@ function createUserInputHeadingFunction() {
     return tableHeadingDiv;
 }
 
+// This function will create the text box inside the table row
 function createTextBoxInsideTableRow() {
     const numberOfTextBox = tableHeading.length;
     const userInputDiv = document.createElement('tr');
@@ -85,13 +96,14 @@ function createTextBoxInsideTableRow() {
     return userInputDiv;
 }
 
+// This function will create the user input row
 function createUserInputRowFunction() {
     const rowNumber = document.createElement('td');
     rowNumber.innerText = userCount;
-
+    // Creating the text box inside the table row
     const userInputDiv = createTextBoxInsideTableRow();
     userInputDiv.prepend(rowNumber);
-
+    // Creating the action row
     const actionRow = createActionRow(addSubmitAndCancelButton = true);
     userInputDiv.appendChild(actionRow);
 
@@ -99,6 +111,7 @@ function createUserInputRowFunction() {
 
 }
 
+// Creating the user input row
 function createMainTable() {
     const tableContainerDiv = document.createElement('div');
     const table = document.createElement('table');
@@ -112,6 +125,7 @@ function createMainTable() {
 
 }
 
+// Starting Point of the application
 function loadMainPage(event) {
     setStatusMessage("Click on create contact button.");
 
@@ -124,10 +138,13 @@ function loadMainPage(event) {
     appNameText.style.fontSize = '2rem';
     appNameDiv.appendChild(appNameText);
 
+    // The user table is created here
     const tableContainerDiv = createMainTable();
 
+    // The create contact button is created here
     const createButtonDiv = document.createElement('div');
     const createButton = document.createElement('button');
+
 
     createButton.innerText = 'Create Contact';
     createButtonDiv.appendChild(createButton);
@@ -140,16 +157,17 @@ function loadMainPage(event) {
 }
 
 
-// Starting Point of the application
+// This function handles the user input row creation, validation and submission
 function createContactFunction(event) {
     setStatusMessage('Please enter User details.');
     if (userCount == 0) {
+        // Creating the heading of the table
         const tableHead = document.querySelector('thead');
         const createUserInputHeading = createUserInputHeadingFunction();
         tableHead.appendChild(createUserInputHeading);
     }
     userCount += 1
-
+    // Creating the user input row
     const tableBody = document.querySelector('tbody');
     const createUserInputRow = createUserInputRowFunction()
     tableBody.appendChild(createUserInputRow);
